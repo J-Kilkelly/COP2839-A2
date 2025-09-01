1187c40188d401ce1e017fef3b6a343675cc9844
John Kilkelly <JKilkelly@live.com>
2025-08-31 23:10:21 -0400

    Aug 31, 23:00

---
+++ COP2839-A2/Controllers/VFRWxController.cs
@@ -0,0 +1,44 @@
+﻿using COP2839_A2.Interfaces;
+using COP2839_A2.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace COP2839_A2.Controllers
+{
+	public class VFRWxController : Controller
+	{
+		private readonly IVFRWxService _vfrwxservice;
+
+		public VFRWxController(IVFRWxService vfrwxservice)
+		{
+			_vfrwxservice = vfrwxservice;
+		}
+
+
+		[HttpGet]
+		public IActionResult CheckVFR
+			(
+			int belowClouds,
+			int aboveClouds,
+			int horizontalCloudSeparation,
+			int visibility
+			)
+		{
+			bool isVFR = _vfrwxservice.IsVFR
+				(
+				belowClouds,
+				aboveClouds,
+				horizontalCloudSeparation,
+				visibility
+				);
+
+			var model = new VFRWxInputModel
+			{
+				BelowClouds = belowClouds,
+				AboveClouds = aboveClouds,
+				HorizontalCloudSeparation = horizontalCloudSeparation,
+				Visibility = visibility,
+				IsVFR = isVFR
+			};
+			return View(isVFR);
+		}
+	}
+}
---
+++ COP2839-A2/Models/VFRWxInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace COP2839_A2.Models
+{
+	public class VFRWxInputModel
+	{
+		public int BelowClouds { get; set; }
+		public int AboveClouds { get; set; }
+		public int HorizontalCloudSeparation { get; set; }
+		public int Visibility { get; set; }
+		public bool IsVFR { get; set; }
+	}
+}
